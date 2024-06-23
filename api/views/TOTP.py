9a84770bbcba89409099efda59b3ab416611e296
YangYiFan <Coolfan1024@gmail.com>
2024-06-23 22:52:44 +0800

    totp限流

--- api/views/TOTP.py
+++ api/views/TOTP.py
@@ -1,4 +1,7 @@
+import time
+
 from django.contrib.auth.models import User
+from django.core.cache import cache
 from pyotp import TOTP
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -10,9 +13,14 @@ from UniBoard.settings import TOTP_SECRET_KEY
 class TOTPDetail(APIView):
 
     def post(self, request):
+        keys = cache.keys("totp_*")
+        if len(keys) >= 5:
+            return Response(status=400, data={"detail": "Too many tries"})
         key = request.data.get("key")
         OTP_code = TOTP(TOTP_SECRET_KEY).now()
         if key != OTP_code:
+            # 失败才记录，成功不限制登录次数
+            cache.set("totp_" + str(int(time.time())), key, 5 * 60)
             return Response(status=400, data={"detail": "Invalid key"})
         users = User.objects.all()
         if users.count() != 1:

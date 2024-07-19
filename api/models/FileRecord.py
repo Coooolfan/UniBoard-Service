f4b2a8859d46939774a8a6741711b297d494cfa1
YangYiFan <Coolfan1024@gmail.com>
2024-07-19 22:21:58 +0800

    fix: FileRecord的描述允许为空

--- api/models/FileRecord.py
+++ api/models/FileRecord.py
@@ -14,7 +14,7 @@ class FileRecord(models.Model):
 
     file_name = models.CharField(max_length=100, verbose_name="文件名")
     file = models.FileField(upload_to='file/', verbose_name="文件")
-    desc = models.TextField(verbose_name="文件描述")
+    desc = models.TextField(blank=True, verbose_name="文件描述")
     permission = models.IntegerField(choices=permission_choices, default=private, verbose_name="权限")
     password = models.CharField(max_length=16, verbose_name="密码", null=True, blank=True, default=None)
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")

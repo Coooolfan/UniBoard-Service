059dc4e9411009a2d9fb07de69a6babb6374beb9
YangYiFan <Coolfan1024@gmail.com>
2024-08-12 15:01:00 +0800

    fix: 数据库迁移脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,7 @@
 
 set -e
 
-upgrade_from_0_2_0() {
-    echo "Upgrading to 0.2.1..."
-#    生成迁移文件
-    python manage.py makemigrations
-#    执行迁移
-    python manage.py migrate
-    echo "0.2.1" > /app/media/version
-}
+CURRENT_VERSION="0.2.2"
 
 # 检查是否需要初始化
 if [ ! -f "/app/media/initialized" ]; then
@@ -18,7 +11,7 @@ if [ ! -f "/app/media/initialized" ]; then
     ls -l /app/api/migrations/
 #    生成随机字符串
     SECRET_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64)
-    echo $SECRET_KEY > /app/media/SECRET_KEY
+    echo "$SECRET_KEY" > /app/media/SECRET_KEY
 #    生成迁移文件
     python manage.py makemigrations
 #    执行迁移
@@ -33,19 +26,20 @@ if [ ! -f "/app/media/initialized" ]; then
 #    创建默认userinfo
     python manage.py loaddata /app/default_userinfo.json
     touch /app/media/initialized
-    echo "0.2.1" > /app/media/version
+    echo "$CURRENT_VERSION" > /app/media/version
 else
     echo "Skipping initialization..."
+    echo "Checking for upgrades..."
 #    检查文件是否存在
-    if [ ! -f "/app/media/version" ]; then
-        INSTALLED_VERSION="0.2.0"
-    else
-        INSTALLED_VERSION=$(cat /app/media/version)
+    INSTALLED_VERSION=$(cat /app/media/version)
+    if [ "$INSTALLED_VERSION" != "$CURRENT_VERSION" ]; then
+      #    生成迁移文件
+      python manage.py makemigrations
+      #    执行迁移
+      python manage.py migrate
+      echo "$CURRENT_VERSION" > /app/media/version
     fi
-#    如果版本号是0.2.0，升级到0.2.1
-    if [ "$INSTALLED_VERSION" = "0.2.0" ]; then
-        upgrade_from_0_2_0
-    fi
+    echo "Launch Django and Celery..."
 fi
 
 # 执行传入的命令（在这里是运行 supervisord）

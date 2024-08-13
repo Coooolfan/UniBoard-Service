fd82a1c9cecc1fa768b5f9f8758b28b03928ef39
YangYiFan <Coolfan1024@gmail.com>
2024-08-13 17:01:49 +0800

    refactor: 使用APIView实现FileRecord相关方法

--- api/views/file_record_views.py
+++ api/views/file_record_views.py
@@ -1,68 +1,68 @@
 from rest_framework import permissions, status
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
-    DestroyModelMixin
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.models import FileRecord
 from api.serializers import FileRecordSerializer
 
 
-class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'size'
-    max_page_size = 100
-
-
-class FileRecordList(GenericAPIView, ListModelMixin, CreateModelMixin):
-    queryset = FileRecord.objects.all().order_by('create_time')
+class FileRecordList(APIView):
+    queryset = FileRecord.objects.all()
     serializer_class = FileRecordSerializer
     permission_classes = (permissions.IsAuthenticated,)
-    pagination_class = CustomPageNumberPagination
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    def get(self, request):
+        queryset = FileRecord.objects.all().order_by('create_time')
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        page = PageNumberPagination()  # 产生一个分页器对象
+        page.page_size = 10  # 默认每页显示的多少条记录
+        page.page_query_param = 'page'  # 默认查询参数名为 page
+        page.page_size_query_param = 'size'  # 前台控制每页显示的最大条数
+        page.max_page_size = 100  # 后台控制显示的最大记录条数
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({'count': queryset.count(), 'results': serializer.data})
+        ret = page.paginate_queryset(queryset, request)
+        serializer = self.serializer_class(ret, many=True)
+        resp = {
+            'count': queryset.count(),
+            'results': serializer.data
+        }
+        return Response(data=resp, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        # 上传文件，文件的share_code是自动生成，参见FileRecordSerializer的create方法
+        s = self.serializer_class(data=request.data)
+        if s.is_valid():
+            s.save()
+            return Response(data=s.data, status=status.HTTP_201_CREATED)
+        return Response(data=s.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class FileRecordDetail(GenericAPIView, RetrieveModelMixin, UpdateModelMixin,
-                       DestroyModelMixin):
+class FileRecordDetail(APIView):
     queryset = FileRecord.objects.all()
     serializer_class = FileRecordSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    lookup_field = 'share_code'
 
     # 此接口只会从文件分享页发起
-    def get(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance is None or instance.permission == FileRecord.Permission.PRIVATE.value:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = self.get_serializer(instance, fields=('file', 'file_name', 'create_time', 'permission', 'desc'))
-        return Response(serializer.data)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-    def get_object(self):
-        queryset = self.filter_queryset(self.get_queryset())
+    def get(self, request, pk):
         try:
-            obj = queryset.get(share_code=self.kwargs[self.lookup_field])
+            file_record = self.queryset.get(share_code=pk)
         except FileRecord.DoesNotExist:
-            return None
-        self.check_object_permissions(self.request, obj)
-        return obj
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        if file_record.permission == FileRecord.Permission.PRIVATE.value and not request.user.is_superuser:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        s = self.serializer_class(file_record, fields=('file', 'file_name', 'create_time', 'permission', 'desc'))
+        return Response(data=s.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        file_record = self.queryset.get(pk=pk)
+        s = self.serializer_class(file_record, data=request.data, partial=True)
+        if s.is_valid():
+            s.save()
+            return Response(data=s.data, status=status.HTTP_200_OK)
+        return Response(data=s.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        file_record = self.queryset.get(pk=pk)
+        file_record.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

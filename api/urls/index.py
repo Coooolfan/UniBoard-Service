7dd2e972cf8f0772daaa6981264e93220e4ead52
YangYiFan <Coolfan1024@gmail.com>
2024-11-02 18:34:17 +0800

    feat: 新增接口允许修改密码

--- api/urls/index.py
+++ api/urls/index.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/change/', TokenChangeDetail.as_view(), name='token_change'),
 
     path('user-info/', UserInfoList.as_view(), name='user-info-list'),
     path('user-info/<int:pk>/', UserInfoDetail.as_view(), name='user-info-detail'),
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -5,4 +5,5 @@ from .hyper_link_views import HyperLinkList, HyperLinkDetail
 from .note_view import NoteList, NoteDetail
 from .short_url_views import ShortUrlList, ShortUrlDetails
 from .sys_config_views import SysConfigList, SysConfigDetail
+from .token_change_view import TokenChangeDetail
 from .user_info_views import UserInfoList, UserInfoDetail

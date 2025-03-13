b1f8394b2861e9cc42b50ebe41e18828be4dce77
YangYiFan <coolfan1024@gmail.com>
2025-03-13 12:59:00 +0800

    feat: 笔记的增删改查

---
+++ src/main/kotlin/com/coooolfan/uniboard/controller/NoteController.kt
@@ -0,0 +1,41 @@
+package com.coooolfan.uniboard.controller
+
+import com.coooolfan.uniboard.model.Note
+import com.coooolfan.uniboard.model.dto.NoteInsert
+import com.coooolfan.uniboard.model.dto.NoteUpdate
+import com.coooolfan.uniboard.repo.NoteRepo
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/note")
+class NoteController(private val repo: NoteRepo) {
+    @GetMapping
+    fun getAllNotes(): List<Note> {
+        return repo.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun getNoteById(@PathVariable id: Long): Note {
+        return repo.findById(id) ?: throw IllegalArgumentException("Note not found")
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun insertNote(@RequestBody insert: NoteInsert) {
+        repo.insert(insert)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteNoteById(@PathVariable id: Long) {
+        repo.deleteById(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateNoteById(@PathVariable id: Long, @RequestBody update: NoteUpdate) {
+        repo.update(update.toEntity { this.id = id })
+    }
+
+}

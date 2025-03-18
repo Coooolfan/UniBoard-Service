0420aa0df29c80d6e934b03428f78f6790b3ff1f
YangYiFan <coolfan1024@gmail.com>
2025-03-18 23:23:44 +0800

    feat: 文件记录的权限和返回对象

--- src/main/kotlin/com/coooolfan/uniboard/controller/FileRecordController.kt
+++ src/main/kotlin/com/coooolfan/uniboard/controller/FileRecordController.kt
@@ -1,10 +1,14 @@
 package com.coooolfan.uniboard.controller
 
+import cn.dev33.satoken.annotation.SaCheckLogin
 import com.coooolfan.uniboard.error.FileRecordException
 import com.coooolfan.uniboard.model.FileRecord
+import com.coooolfan.uniboard.model.by
 import com.coooolfan.uniboard.model.dto.FileRecordInsert
 import com.coooolfan.uniboard.model.dto.FileRecordUpdate
 import com.coooolfan.uniboard.service.FileRecordService
+import org.babyfish.jimmer.client.FetchBy
+import org.babyfish.jimmer.sql.kt.fetcher.newFetcher
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
@@ -12,31 +16,40 @@ import org.springframework.web.multipart.MultipartFile
 @RequestMapping("/api/file-record")
 class FileRecordController(private val service: FileRecordService) {
     @GetMapping
+    @SaCheckLogin
     fun getAllFileRecords(): List<FileRecord> {
         return service.findAll()
     }
 
     @PutMapping("/{id}")
+    @SaCheckLogin
     fun updateFileRecordById(@PathVariable id: Long, @RequestBody update: FileRecordUpdate) {
         service.update(update.toEntity { this.id = id })
     }
 
     @GetMapping("/{id}")
-    fun getFileRecordById(@PathVariable id: Long): FileRecord {
-        return service.findById(id) ?: throw IllegalArgumentException("FileRecord not found")
+    fun getFileRecordById(@PathVariable id: Long): @FetchBy("PUBLIC_FILERECORD") FileRecord {
+        return service.findById(id, PUBLIC_FILERECORD) ?: throw IllegalArgumentException("FileRecord not found")
     }
 
     @DeleteMapping("/{id}")
+    @SaCheckLogin
     fun deleteFileRecordById(@PathVariable id: Long) {
         service.deleteById(id)
     }
 
     @PostMapping("/files")
+    @SaCheckLogin
     @Throws(FileRecordException.EmptyPassword::class)
     fun uploadFile(
         @RequestPart insert: FileRecordInsert,
         @RequestPart file: MultipartFile
     ): FileRecord {
         return service.insert(insert, file)
+    }
+
+    private val PUBLIC_FILERECORD = newFetcher(FileRecord::class).by {
+        allScalarFields()
+        password(false)
     }
 }
--- src/main/kotlin/com/coooolfan/uniboard/service/FileRecordService.kt
+++ src/main/kotlin/com/coooolfan/uniboard/service/FileRecordService.kt
@@ -6,6 +6,7 @@ import com.coooolfan.uniboard.model.FileRecordVisibility
 import com.coooolfan.uniboard.model.dto.FileRecordInsert
 import com.coooolfan.uniboard.repo.FileRecordRepo
 import com.coooolfan.uniboard.utils.getHashedString
+import org.babyfish.jimmer.sql.fetcher.Fetcher
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.nio.file.Paths
@@ -16,7 +17,7 @@ class FileRecordService(private val repo: FileRecordRepo) {
     fun findAll() = repo.findAll()
     fun deleteById(id: Long) = repo.deleteById(id)
     fun update(update: FileRecord) = repo.update(update)
-    fun findById(id: Long): FileRecord? = repo.findById(id)
+    fun findById(id: Long, fetcher: Fetcher<FileRecord>): FileRecord? = repo.findById(id, fetcher)
 
     fun insert(insert: FileRecordInsert, file: MultipartFile): FileRecord {
         if (insert.visibility == FileRecordVisibility.PASSWORD && insert.password.trim().isEmpty())

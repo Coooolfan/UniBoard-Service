4ef03ea34522422f10df60e20be52f90ec7151b6
YangYiFan <coolfan1024@gmail.com>
2025-03-22 15:58:07 +0800

    feat: Caffeine的引入与临时短链的实现

--- src/main/kotlin/com/coooolfan/uniboard/service/FileRecordService.kt
+++ src/main/kotlin/com/coooolfan/uniboard/service/FileRecordService.kt
@@ -3,9 +3,12 @@ package com.coooolfan.uniboard.service
 import com.coooolfan.uniboard.error.FileRecordException
 import com.coooolfan.uniboard.model.FileRecord
 import com.coooolfan.uniboard.model.FileRecordVisibility
+import com.coooolfan.uniboard.model.dto.FileRecordDirectLinkCreate
+import com.coooolfan.uniboard.model.dto.FileRecordDirectLinkResp
 import com.coooolfan.uniboard.model.dto.FileRecordInsert
 import com.coooolfan.uniboard.repo.FileRecordRepo
 import com.coooolfan.uniboard.utils.getHashedString
+import com.github.benmanes.caffeine.cache.Cache
 import org.babyfish.jimmer.sql.fetcher.Fetcher
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -13,7 +16,10 @@ import java.nio.file.Paths
 import java.util.*
 
 @Service
-class FileRecordService(private val repo: FileRecordRepo) {
+class FileRecordService(
+    private val repo: FileRecordRepo,
+    private val directLinkCache: Cache<String, Long>
+) {
     fun findAll() = repo.findAll()
     fun deleteById(id: Long) = repo.deleteById(id)
     fun update(update: FileRecord) = repo.update(update)
@@ -34,5 +40,15 @@ class FileRecordService(private val repo: FileRecordRepo) {
                 this.file { this.filepath = relativePath.toString() }
             },
         ).modifiedEntity
+    }
+
+    fun createDirectLink(create: FileRecordDirectLinkCreate): FileRecordDirectLinkResp {
+        val uuid = UUID.randomUUID().toString()
+        // Store the mapping between UUID and file ID in the cache, expires after 5 minutes
+        directLinkCache.put(uuid, create.id)
+        return FileRecordDirectLinkResp(
+            id = create.id,
+            directUUID = uuid,
+        )
     }
 }

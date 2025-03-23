8e4be2e37f88ef99e6a626913baf1f33d1a34396
YangYiFan <coolfan1024@gmail.com>
2025-03-23 19:46:47 +0800

    fix: 抛出可能的异常

--- src/main/kotlin/com/coooolfan/uniboard/controller/TokenController.kt
+++ src/main/kotlin/com/coooolfan/uniboard/controller/TokenController.kt
@@ -2,6 +2,7 @@ package com.coooolfan.uniboard.controller
 
 import cn.dev33.satoken.annotation.SaCheckLogin
 import cn.dev33.satoken.stp.StpUtil
+import com.coooolfan.uniboard.error.CommonException
 import com.coooolfan.uniboard.model.dto.ProfileLogin
 import com.coooolfan.uniboard.service.ProfileService
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/token")
 class TokenController(private val service: ProfileService) {
     @GetMapping
+    @Throws(CommonException.AuthenticationFailed::class)
     fun getToken(login: ProfileLogin) {
         service.checkLogin(login)
     }

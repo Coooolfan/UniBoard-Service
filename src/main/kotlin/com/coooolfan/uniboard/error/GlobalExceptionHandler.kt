b331eed1a815bdadb759bff28e6bce21fef47ed2
YangYiFan <coolfan1024@gmail.com>
2025-03-23 15:19:26 +0800

    feat: 更好的异常状态码

--- src/main/kotlin/com/coooolfan/uniboard/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/coooolfan/uniboard/error/GlobalExceptionHandler.kt
@@ -1,58 +1,41 @@
 package com.coooolfan.uniboard.error
 
 import cn.dev33.satoken.exception.NotLoginException
-import org.springframework.http.HttpStatus
-import org.springframework.http.ProblemDetail
+import org.babyfish.jimmer.error.CodeBasedRuntimeException
 import org.springframework.http.ResponseEntity
-import org.springframework.web.ErrorResponseException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import java.net.URI
-
 
 @RestControllerAdvice
-class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
-
-    @ExceptionHandler(
-        NotLoginException::class,
-        CommonException.AuthenticationFailed::class
-    )
-    fun handleAuthenticationFailed(ex: Exception, request: WebRequest): ResponseEntity<Any>? {
-        val problemDetail = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED)
-        problemDetail.type = URI.create("https://www.google.com/search?q=401+Unauthorized")
-        val errorResponseException =
-            ErrorResponseException(
-                HttpStatus.UNAUTHORIZED,
-                problemDetail,
-                ex.cause
-            )
-        return handleExceptionInternal(
-            errorResponseException,
-            errorResponseException.body,
-            errorResponseException.headers,
-            errorResponseException.statusCode,
-            request
-        )
+class GlobalExceptionHandler {
+    @ExceptionHandler(CodeBasedRuntimeException::class)
+    fun handle(ex: CodeBasedRuntimeException): ResponseEntity<Map<String, Any>> {
+        val statusCode = when (ex.code) {
+            ProfileErrorCode.SYSTEM_UNINITIALIZED.toString() -> 404
+            ProfileErrorCode.SYSTEM_ALREADY_INITIALIZED.toString() -> 403
+            CommonErrorCode.NOT_FOUND.toString() -> 404
+            CommonErrorCode.AUTHENTICATION_FAILED.toString() -> 401
+            else -> 500
+        }
+        return ResponseEntity
+            .status(statusCode)
+            .body(resultMap(ex))
     }
 
-    @ExceptionHandler(CommonException.NotFound::class)
-    fun handleNotFound(ex: Exception, request: WebRequest): ResponseEntity<Any?>? {
-        val problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND)
-        problemDetail.type = URI.create("https://www.google.com/search?q=404+Not+Found")
-        val errorResponseException =
-            ErrorResponseException(
-                HttpStatus.NOT_FOUND,
-                problemDetail,
-                ex.cause
-            )
-        return handleExceptionInternal(
-            errorResponseException,
-            errorResponseException.body,
-            errorResponseException.headers,
-            errorResponseException.statusCode,
-            request
-        )
+
+    @ExceptionHandler(NotLoginException::class)
+    fun handleAuthenticationFailed(ex: NotLoginException, request: WebRequest): ResponseEntity<Any>? {
+        return ResponseEntity
+            .status(201)
+            .body(resultMap(CommonException.AuthenticationFailed()))
     }
+
+    protected fun resultMap(ex: CodeBasedRuntimeException): Map<String, Any> {
+        val resultMap: MutableMap<String, Any> = LinkedHashMap()
+        resultMap["family"] = ex.family
+        resultMap["code"] = ex.code
+        return resultMap
+    }
+
 }

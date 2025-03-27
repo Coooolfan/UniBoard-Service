4dfebbe2f8428bb40c725c16526548e0183d87a5
YangYiFan <coolfan1024@gmail.com>
2025-03-27 22:46:57 +0800

    feat: 短链服务支持跳转

--- src/main/kotlin/com/coooolfan/uniboard/controller/ShortUrlController.kt
+++ src/main/kotlin/com/coooolfan/uniboard/controller/ShortUrlController.kt
@@ -6,6 +6,7 @@ import com.coooolfan.uniboard.model.ShortUrl
 import com.coooolfan.uniboard.model.dto.ShortUrlInsert
 import com.coooolfan.uniboard.repo.ShortUrlRepo
 import com.coooolfan.uniboard.utils.getHashedString
+import jakarta.servlet.http.HttpServletResponse
 import org.babyfish.jimmer.Page
 import org.babyfish.jimmer.spring.repo.PageParam
 import org.springframework.http.HttpStatus
@@ -22,15 +23,27 @@ class ShortUrlController(private val repo: ShortUrlRepo) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun insertShortUrl(@RequestBody insert: ShortUrlInsert):ShortUrl {
+    fun insertShortUrl(@RequestBody insert: ShortUrlInsert): ShortUrl {
         return repo.insert(insert.toEntity { this.shortUrl = getHashedString(insert.longUrl) }).modifiedEntity
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteShortUrl(@PathVariable(value = "id") id: Long) {
-        if(repo.deleteById(id)!=1){
+        if (repo.deleteById(id) != 1) {
             throw CommonException.NotFound()
         }
+    }
+}
+
+@RestController
+@RequestMapping("/s")
+@ResponseStatus(HttpStatus.TEMPORARY_REDIRECT)
+class RedirectController(private val repo: ShortUrlRepo) {
+    @GetMapping("/{shortUrlCode}")
+    fun redirect(@PathVariable(value = "shortUrlCode") shortUrlCode: String, resp: HttpServletResponse) {
+        val entity = repo.findByShortUrlCode(shortUrlCode) ?: throw CommonException.NotFound()
+        resp.sendRedirect(entity.longUrl)
+        resp.flushBuffer()
     }
 }
